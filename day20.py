d57853906660020c856d47870e6b786635257b3d
Nico Freundt <nico.freundt@protonmail.com>
2023-12-20 23:51:37 +0100

    Refactoring of day twenty

--- day20.py
+++ day20.py
@@ -1,6 +1,12 @@
 import re
+import math
 
 FILE = "input.txt"
+BUTTON_CLICKS = 1000
+
+R_BROADCAST = 'broadcaster -> (?>[a-z]+, )*(?>[a-z]+)'
+R_FLIPFLOP = '%[a-z]+ -> (?>[a-z]+, )*(?>[a-z]+)'
+R_CONJUNCTION = '&[a-z]+ -> (?>[a-z]+, )*(?>[a-z]+)'
 
 def parse_to_map(modules):
     return {(matches:=re.findall('[a-z]+', module))[0]: matches[1:] for module in modules}
@@ -9,93 +15,112 @@ if __name__ == "__main__":
     # part one
     with open(FILE) as file:
         module_list = file.read()
-        broadcaster = parse_to_map(re.findall('broadcaster -> (?>[a-z]+, )*(?>[a-z]+)', module_list))
-        flip_flops = parse_to_map(re.findall('%[a-z]+ -> (?>[a-z]+, )*(?>[a-z]+)', module_list))
-        conjunctions = parse_to_map(re.findall('&[a-z]+ -> (?>[a-z]+, )*(?>[a-z]+)', module_list))
+        
+        broadcaster = parse_to_map(re.findall(R_BROADCAST, module_list))
+        flip_flops = parse_to_map(re.findall(R_FLIPFLOP, module_list))
+        conjunctions = parse_to_map(re.findall(R_CONJUNCTION, module_list))
+        
+        typed_modules = dict(flip_flops, **conjunctions)
+
         active_flops = []
-        all_modules = flip_flops.copy()
-        all_modules.update(conjunctions)
-        con_input = {con: {k: 0 for k, _ in filter(lambda x: con in x[1], all_modules.items())} for con in conjunctions}
-        all_signals = [1000, 0]
-        for _ in range(1000):
+        conjunction_input_defaults = {con: {k: 0 for k, _ in filter(lambda x: con in x[1], typed_modules.items())} for con in conjunctions}
+        
+        sent_signals = [BUTTON_CLICKS, 0]
+        
+        for _ in range(BUTTON_CLICKS):
             cur_signals = [('broadcaster', 0, signal) for signal in broadcaster['broadcaster']]
+            
             while cur_signals:
-                last_module, pulse, cur_signal = cur_signals.pop(0)
-                all_signals[pulse] += 1
-                if cur_signal not in all_modules:
+                last_module, pulse, cur_module = cur_signals.pop(0)
+                
+                sent_signals[pulse] += 1
+                
+                if cur_module not in typed_modules:
                     continue
-                if cur_signal in conjunctions:
-                    con_input[cur_signal][last_module] = pulse
-                cur_modules = flip_flops[cur_signal] if cur_signal in flip_flops else conjunctions[cur_signal]
-                if cur_signal in flip_flops and pulse == 0:
-                        for module in cur_modules:
-                            cur_signals.append((cur_signal, cur_signal not in active_flops, module))
-                elif cur_signal in conjunctions:
-                    if len(con_input[cur_signal]) == 1:
-                        for module in cur_modules:
-                            cur_signals.append((cur_signal, 0 if pulse else 1, module))
+                
+                next_modules = typed_modules[cur_module]
+                
+                if cur_module in flip_flops and not pulse:
+                    for module in next_modules:
+                        cur_signals.append((cur_module, cur_module not in active_flops, module))
+                
+                if cur_module in conjunctions:
+                    conjunction_input_defaults[cur_module][last_module] = pulse
+                    
+                    if pulse and len(conjunction_input_defaults[cur_module]) == 1:
+                        next_pulse = 0
+                    elif all(conjunction_input_defaults[cur_module].values()):
+                        next_pulse = 0
                     else:
-                        if all(v for v in con_input[cur_signal].values()):
-                            for module in cur_modules:
-                                cur_signals.append((cur_signal, 0, module))
-                        else:
-                            for module in cur_modules:
-                                cur_signals.append((cur_signal, 1, module))
+                        next_pulse = 1
+                        
+                    for module in next_modules:
+                        cur_signals.append((cur_module, next_pulse, module))
+                
                 if last_module in flip_flops:
-                    if pulse == 1:
-                        active_flops.append(last_module)
-                    elif pulse == 0:
-                        active_flops.remove(last_module)
-        print("Part 1:", all_signals[0] * all_signals[1])
+                    active_flops.append(last_module) if pulse else active_flops.remove(last_module)
+
+        print("Part 1:", math.prod(sent_signals))
 
     # part two 
     with open(FILE) as file:
         module_list = file.read()
-        broadcaster = parse_to_map(re.findall('broadcaster -> (?>[a-z]+, )*(?>[a-z]+)', module_list))
-        flip_flops = parse_to_map(re.findall('%[a-z]+ -> (?>[a-z]+, )*(?>[a-z]+)', module_list))
-        conjunctions = parse_to_map(re.findall('&[a-z]+ -> (?>[a-z]+, )*(?>[a-z]+)', module_list))
+
+        broadcaster = parse_to_map(re.findall(R_BROADCAST, module_list))
+        flip_flops = parse_to_map(re.findall(R_FLIPFLOP, module_list))
+        conjunctions = parse_to_map(re.findall(R_CONJUNCTION, module_list))
+        
+        typed_modules = dict(flip_flops, **conjunctions)
+
         active_flops = []
-        all_modules = flip_flops.copy()
-        all_modules.update(conjunctions)
-        con_input = {con: {k: 0 for k, _ in filter(lambda x: con in x[1], all_modules.items())} for con in conjunctions}
-        pressed_button = 0
-        no_low_rx = True
-        module_before_rx = [k for k, _ in filter(lambda x: 'rx' in x[1], all_modules.items())][0]
-        end_state = con_input[module_before_rx].copy()
-        while no_low_rx:
-            if all(val > 0 for val in end_state.values()):
+        conjunction_input_defaults = {con: {k: 0 for k, _ in filter(lambda x: con in x[1], typed_modules.items())} for con in conjunctions}
+        
+        rx_predecessor = [k for k, _ in filter(lambda x: 'rx' in x[1], typed_modules.items())][0]
+        low_rx_state = conjunction_input_defaults[rx_predecessor].copy()
+        
+        low_rx = False
+        button_clicks = 0
+        
+        while not low_rx:
+            if all(low_rx_state.values()):
+                # Found values for all relevant modules
                 break
-            pressed_button += 1
+            
+            button_clicks += 1
             cur_signals = [('broadcaster', 0, signal) for signal in broadcaster['broadcaster']]
+            
             while cur_signals:
-                last_module, pulse, cur_signal = cur_signals.pop(0)
-                if last_module in con_input[module_before_rx] and pulse == 1:
-                    end_state[last_module] = pressed_button
-                if cur_signal not in all_modules:
-                    if cur_signal == 'rx' and pulse == 0:
-                        no_low_rx == False
+                last_module, pulse, cur_module = cur_signals.pop(0)
+                
+                if cur_module not in typed_modules:
+                    if cur_module == 'rx' and not pulse:
+                        # hopefully
+                        low_rx == True
                     continue
-                if cur_signal in conjunctions:
-                    con_input[cur_signal][last_module] = pulse
-                cur_modules = flip_flops[cur_signal] if cur_signal in flip_flops else conjunctions[cur_signal]
-                if cur_signal in flip_flops and pulse == 0:
-                    for module in cur_modules:
-                        cur_signals.append((cur_signal, cur_signal not in active_flops, module))
-                elif cur_signal in conjunctions:
-                    if len(con_input[cur_signal]) == 1:
-                        for module in cur_modules:
-                            cur_signals.append((cur_signal, 0 if pulse else 1, module))
+                
+                next_modules = typed_modules[cur_module]
+                
+                if last_module in conjunction_input_defaults[rx_predecessor] and pulse:
+                    low_rx_state[last_module] = button_clicks
+                
+                if cur_module in flip_flops and not pulse:
+                    for module in next_modules:
+                        cur_signals.append((cur_module, cur_module not in active_flops, module))
+
+                if cur_module in conjunctions:
+                    conjunction_input_defaults[cur_module][last_module] = pulse
+                    
+                    if pulse and len(conjunction_input_defaults[cur_module]) == 1:
+                        next_pulse = 0
+                    elif all(conjunction_input_defaults[cur_module].values()):
+                        next_pulse = 0
                     else:
-                        if all(v for v in con_input[cur_signal].values()):
-                            for module in cur_modules:
-                                cur_signals.append((cur_signal, 0, module))
-                        else:
-                            for module in cur_modules:
-                                cur_signals.append((cur_signal, 1, module))
+                        next_pulse = 1
+
+                    for module in next_modules:
+                        cur_signals.append((cur_module, next_pulse, module))
+                
                 if last_module in flip_flops:
-                    if pulse == 1:
-                        active_flops.append(last_module)
-                    elif pulse == 0:
-                        active_flops.remove(last_module)
-        x, m, a, s = end_state.values()
-        print("Part 2:", x*m*a*s)
+                    active_flops.append(last_module) if pulse else active_flops.remove(last_module)
+
+        print("Part 2:", math.lcm(*low_rx_state.values()))
